620d1cfe2fcbe2d63fa7e2ba0a506670ccf31072
ponder-j <a2140316092@qq.com>
2026-01-30 22:00:04 +0800

    Auto update at 2026/01/30 22:00:04

--- 0x03_SlidingWin/Ex3_无重复字符的最长子串.py
+++ 0x03_SlidingWin/Ex3_无重复字符的最长子串.py
@@ -3,9 +3,30 @@ from typing import List, Dict, Optional, Set
 # Main Logic
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        return
+        l = r = 0
+        cnt = 0
+        max_cnt = 0
+        cur_set = set()
+        n = len(s)
+        if n == 0 or n == 1:
+            return n
+        while l < n:
+            while r < n:
+                if s[r] not in cur_set:
+                    cur_set.add(s[r])
+                    cnt += 1
+                    r += 1
+                else:
+                    break
+            cur_set.remove(s[l])
+            max_cnt = max(max_cnt, cnt)
+            cnt -= 1
+            l += 1
+        return max_cnt
 
 # 思路总结
+# 滑动窗口，并且需要用到哈希表结构，用于快速判断是否存在重复元素
+# 为什么可以“滑动”，也即为什么 r 指针永远向右移动而不需要回头判断？因为 r 能走到这一步说明之前都没有重复的，所以不需要回头
 
 # Instantiation
 if __name__ == '__main__':
@@ -13,7 +34,7 @@ if __name__ == '__main__':
     sol = Solution()
     
     # 构造测试用例
-    testcase = "abcabcbb"
+    testcase = "au"
     
     # 调用方法并打印结果
     result = sol.lengthOfLongestSubstring(testcase)
---
+++ 0x03_SlidingWin/Ex438_找到字符串中所有字母异位词.py
@@ -0,0 +1,20 @@
+from typing import List, Dict, Optional, Set
+
+# Main Logic
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        
+
+# 思路总结
+
+# Instantiation
+if __name__ == '__main__':
+    # 实例化 Solution 类
+    sol = Solution()
+    
+    # 构造测试用例
+    testcase = []
+    
+    # 调用方法并打印结果
+    result = sol.findAnagrams(testcase)
+    print(f"输出结果: {result}")
