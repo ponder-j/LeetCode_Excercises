36db61fd153b587d174071040867dcfd9d1c9d99
ponder-j <a2140316092@qq.com>
2026-02-09 21:08:07 +0800

    Auto update at 2026/02/09 21:08:07

---
+++ 0x06_Matrix/Ex73_矩阵置零.py
@@ -0,0 +1,22 @@
+from typing import List, Dict, Optional, Set
+
+# Main Logic
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+# 思路总结
+
+# Instantiation
+if __name__ == '__main__':
+    # 实例化 Solution 类
+    sol = Solution()
+    
+    # 构造测试用例
+    testcase = []
+    
+    # 调用方法并打印结果
+    result = sol.setZeroes(testcase)
+    print(f"输出结果: {result}")
