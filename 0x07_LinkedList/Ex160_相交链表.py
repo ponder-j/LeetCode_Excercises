4dc9ded909fd6f5e1fe99f39f97e26ebd3d1e2ce
ponder-j <a2140316092@qq.com>
2026-04-30 13:12:29 +0800

    Auto update at 2026/04/30 13:12:28

--- 0x07_LinkedList/Ex160_相交链表.py
+++ 0x07_LinkedList/Ex160_相交链表.py
@@ -5,11 +5,22 @@ from typing import List, Dict, Optional, Set
 class ListNode:
     def __init__(self, x):
         self.val = x
-        self.next = None
+        self.next: Optional['ListNode'] = None
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        
+        pass_dict = set()
+        cur = headA
+        while cur is not None:
+            pass_dict.add(cur)
+            cur = cur.next
+        cur = headB
+        while cur is not None:
+            if cur in pass_dict:
+                return cur
+            else:
+                cur = cur.next
+        return None
 
 # 思路总结
 
@@ -19,8 +30,23 @@ if __name__ == '__main__':
     sol = Solution()
     
     # 构造测试用例
-    testcase = []
+    a1 = ListNode(4)
+    a2 = ListNode(1)
+    a3 = ListNode(8)
+    a4 = ListNode(4)
+    a5 = ListNode(5)
+    a6 = ListNode(5)
+    a7 = ListNode(6)
+    a8 = ListNode(1)
+
+    a1.next = a2
+    a2.next = a3
+    a3.next = a4
+    a4.next = a5
+    a6.next = a7
+    a7.next = a8
+    a8.next = a3
     
     # 调用方法并打印结果
-    result = sol.fun(testcase)
-    print(f"输出结果: {result}")
+    result = sol.getIntersectionNode(a1, a6)
+    print(f"输出结果: {result.val}")
