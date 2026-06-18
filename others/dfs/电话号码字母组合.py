a3862d8cff09cde20e4b853637aeee2bd5a3a5b4
ponder-j <a2140316092@qq.com>
2026-06-18 11:02:41 +0800

    Auto update at 2026/06/18 11:02:40

--- others/dfs/电话号码字母组合.py
+++ others/dfs/电话号码字母组合.py
@@ -2,18 +2,27 @@ from typing import List, Dict, Optional, Set
 import sys
 
 dic_n2l = {
-    "2": "",
-    "3": "",
-    "4": "",
-    "5": "",
-    "6": "",
-    "7": "",
-    "8": "",
-    "9": "",
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz",
 }
 
 def combin(a: str) -> None:
-    pass
+    n = len(a)
+
+    def dfs(sentence: str, step: int):
+        if step == n:
+            print(sentence)
+            return
+        
+        for letter in dic_n2l[a[step]]:
+
+
 
 def main():
     nums = sys.stdin.readline().strip()
