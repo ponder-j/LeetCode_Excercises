b168659fa7626720cdb5e3db49d93a2405d01baa
ponder-j <a2140316092@qq.com>
2026-09-03 00:00:00 +0800

    Auto update at 2026/09/03 00:00:00

--- 0x09_Graph/Ex200_岛屿数量.py
+++ 0x09_Graph/Ex200_岛屿数量.py
@@ -3,8 +3,16 @@ from typing import List, Dict, Optional, Set
 # Main Logic
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    sink
+
 
 # 思路总结
+
 
 # Instantiation
 if __name__ == '__main__':
